a9301c192596ec148c11d58454fb59f5dc490901
Kazuya Okada <kazuya-o@is.naist.jp>
2015-04-16 14:01:45 +0900

    add netflow importer

---
+++ importer/netflow/README.md
@@ -0,0 +1,25 @@
+# Module Name
+- netflow importer
+
+# Input Dataset
+- binary netflow data captured by nfcapd (URL: http://nfdump.sourceforge.net/)
+	- file name format: nfcapd.< YYYYMMDDHHMM >
+
+# Description 
+- The script imports daily netflow data which is gathered by nfcapd on a MATATABI table. The script use nfdump tool in the data conversion. The table definition is available on "schema" directory.
+
+
+# Usage
+- one shot (impport netflow files which gathered at the 1st argument date < YYYYMMDD >) 
+
+```shell: 
+# sh put-netflow.sh <YYYYMMDD> <NETFLOW TABLE NAME>
+```
+
+- cron
+
+```
+* 11 * * * ~/put-sflow.sh `date --date '1 days ago' "+\%Y\%m\%d"` <NETFLOW TABLE NAME>
+```
+
+
---
+++ importer/netflow/scripts/put-netflow.sh
@@ -0,0 +1,30 @@
+# DESCRIPTION :
+# This script convert netflow log to a csv format file.
+# USAGE : $sh put-netflow.sh <YYYYMMDD> <NETFLOW TABLE NAME>
+#!/bin/sh
+
+# remove temporally file
+rm -f ./tmp/$1.csv
+
+
+for dataFile in `ls <NETFLOW DATA DIRECTORPY PATH>/nfcapd.$1*`
+do 
+# convert nfcapd file into csv format
+/home/hadoop/bin/nfdump-necoma -r $dataFile -o csv |tail -n +4 >> ./tmp/$1.csv
+done
+
+# compress csv file 
+lzop -f -U ./tmp/$1.csv
+# import compressed file on the hive table
+hive -S -e "load data local inpath './tmp/$1.csv.lzo' overwrite into table $2 partition(dt='$1');"
+rm -f ./tmp/$1.csv
+
+# convert text data on the hive table into rcfile format
+hive -S \
+ -hiveconf hive.exec.compress.intermediate=true \
+ -hiveconf hive.exec.compress.output=true \
+ -hiveconf mapred.output.compression.type=BLOCK \
+ -hiveconf mapred.output.compression.codec=org.apache.hadoop.io.compress.LzoCodec \
+ -hiveconf hive.exec.dynamic.partition=true \
+ -hiveconf hive.exec.dynamic.partition.mode=nonstrict \
+ -e "insert overwrite table $2_rcfile partition(dt) select * from $2 where dt='$1' DISTRIBUTE by dt;"
