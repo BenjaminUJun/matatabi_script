0bc83d386009977c7b8dd95c5d2756ead7cdac0c
Kazuya Okada <kazuya-o@is.naist.jp>
2015-04-28 16:28:57 +0900

    fix misspells

--- importer/spam/scripts/HiveInit.sh
+++ importer/spam/scripts/HiveInit.sh
@@ -13,6 +13,7 @@ cd $DIST_DIR
 sed -i -r -e "s@SPAM_TABLE_NAME@$MY_TABLE_NAME@" schema/*.sql
 sed -i -r -e "s@SPAM_TABLE_NAME@$MY_TABLE_NAME@" scripts/put-spam-hdfs.sh
 sed -i -r -e "s@SPAM_DATA_DIRECTORY@$SPAM_DATA_DIRECTORY@" scripts/put-spam-hdfs.sh
+sed -i -r -e "s@SPAM_DATA_ANALYSIS_DIRECTORY@$SPAM_DATA_ANALYSIS_DIRECTORY@" scripts/put-spam-hdfs.sh
 sed -i -r -e "s@SERVER_NAME@$SERVER_NAME@" scripts/put-spam-hdfs.sh
 
 # create hive schema
--- importer/spam/scripts/put-spam-hdfs.sh
+++ importer/spam/scripts/put-spam-hdfs.sh
@@ -5,5 +5,5 @@
 date=$1
 
 /usr/bin/python spam-parse.py SPAM_DATA_DIRECTORY $date
-/usr/local/hive-0.11.0/bin/hive -S -e "LOAD DATA LOCAL INPATH 'SPAM_DATA_ANALYSIS_DIRECTORY/${date}.output' OVERWRITE INTO TABLE SPAM_TABLE_NAME> partition (dt='${date}', sv='SERVER_NAME');" > /dev/null
+/usr/local/hive-0.11.0/bin/hive -S -e "LOAD DATA LOCAL INPATH 'SPAM_DATA_ANALYSIS_DIRECTORY/${date}.output' OVERWRITE INTO TABLE SPAM_TABLE_NAME partition (dt='${date}', sv='SERVER_NAME');" > /dev/null
 
