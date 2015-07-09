eef1d73880c9b5d64f418a4efe90acc5984249de
Hajime Tazaki <tazaki@sfc.wide.ad.jp>
2015-07-09 21:44:57 +0900

    fix netflow table location

--- importer/netflow/scripts/HiveInit.sh
+++ importer/netflow/scripts/HiveInit.sh
@@ -4,14 +4,14 @@ DIST_DIR=`dirname $0`/../
 
 NFDUMP_PATH=/usr/sbin/nfdump
 MY_TABLE_NAME=netflow
-NETFLOW_DATA_DIRECTORPY=/tmp/data/netflow/
+NETFLOW_DATA_DIRECTORY=$(DIST_DIR)/../data/
 
 cd $DIST_DIR
 
 # modify settings
 sed -i -r -e "s@NETFLOW_TABLE_NAME@$MY_TABLE_NAME@" schema/*.sql
 sed -i -r -e "s@NETFLOW_TABLE_NAME@$MY_TABLE_NAME@" scripts/put-netflow.sh
-sed -i -r -e "s@NETFLOW_DATA_DIRECTORY@$NFDUMP_PATH@" scripts/put-netflow.sh
+sed -i -r -e "s@NETFLOW_DATA_DIRECTORY@$NETFLOW_DATA_DIRECTORY@" scripts/put-netflow.sh
 sed -i -r -e "s@NFDUMP_PATH@$NFDUMP_PATH@" scripts/put-netflow.sh
 
 # create hive schema
--- importer/netflow/scripts/put-netflow.sh
+++ importer/netflow/scripts/put-netflow.sh
@@ -4,26 +4,26 @@
 #!/bin/sh
 
 # remove temporally file
-rm -f ./tmp/$1.csv
+rm -f /tmp/$1.csv
 
-for dataFile in `ls NETFLOW_DATA_DIRECTORPY/nfcapd.$1*`
+for dataFile in `ls NETFLOW_DATA_DIRECTORY/nfcapd.$1*`
 do 
 # convert nfcapd file into csv format
-NFDUMP_PATH -r $dataFile -o csv |tail -n +4 >> ./tmp/$1.csv
+NFDUMP_PATH -r $dataFile -o csv |tail -n +4 >> /tmp/$1.csv
 done
 
 # compress csv file 
-lzop -f -U ./tmp/$1.csv
+lzop -f -U /tmp/$1.csv
 # import compressed file on the hive table
-hive -S -e "load data local inpath './tmp/$1.csv.lzo' overwrite into table NETFLOW_TABLE_NAME partition(dt='$1');"
-rm -f ./tmp/$1.csv
+hive -S -e "load data local inpath '/tmp/$1.csv.lzo' overwrite into table NETFLOW_TABLE_NAME partition(dt='$1');"
+rm -f /tmp/$1.csv*
 
 # convert text data on the hive table into rcfile format
-hive -S \
- -hiveconf hive.exec.compress.intermediate=true \
- -hiveconf hive.exec.compress.output=true \
- -hiveconf mapred.output.compression.type=BLOCK \
- -hiveconf mapred.output.compression.codec=org.apache.hadoop.io.compress.LzoCodec \
- -hiveconf hive.exec.dynamic.partition=true \
- -hiveconf hive.exec.dynamic.partition.mode=nonstrict \
- -e "insert overwrite table $2_rcfile partition(dt) select * from $2 where dt='$1' DISTRIBUTE by dt;"
+# hive -S \
+#  -hiveconf hive.exec.compress.intermediate=true \
+#  -hiveconf hive.exec.compress.output=true \
+#  -hiveconf mapred.output.compression.type=BLOCK \
+#  -hiveconf mapred.output.compression.codec=org.apache.hadoop.io.compress.LzoCodec \
+#  -hiveconf hive.exec.dynamic.partition=true \
+#  -hiveconf hive.exec.dynamic.partition.mode=nonstrict \
+#  -e "insert overwrite table $2_rcfile partition(dt) select * from $2 where dt='$1' DISTRIBUTE by dt;"
